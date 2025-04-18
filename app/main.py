39a551d07ba5a8abdef58014edef243eb4c492be
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2025-04-18 21:52:41 +0500

    feat: registred tortoise ORM to API

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from api.routes.todo import todo_router
 
 app = FastAPI()
 app.include_router(todo_router)
+register_tortoise(
+    app=app,
+    db_url="sqlite://todo.db",
+    add_exception_handlers=True,
+    generate_schemas=True,
+    modules={
+        "models": [
+            "api.models.todo_model",
+        ],
+    },
+)
 
 
 @app.get("/")
